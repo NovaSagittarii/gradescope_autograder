1dbe6d3aef0a046d86c2bd783ee8eca91b4c4819
NovaSagittarii <novasagittarii@gmail.com>
2025-10-30 03:11:55 -0600

    apply no-read to all files after running model solution

--- setup.sh
+++ setup.sh
@@ -5,11 +5,6 @@ set -euo pipefail
 apt install -y g++ python3 python3-pip
 pip3 install pyseccomp
 
-
-### Setup permissions
-find / -type f -exec chmod 640 {}
-adduser student --no-create-home --disabled-password --gecos ""
-
 ### Generating answer files (ahead of time for faster grading). ###
 cd /autograder/source/src
 python3 compile.py solution
@@ -18,10 +13,6 @@ python3 exec.py solution
 # clean up side-effect of running compile.py and exec.py
 echo > /autograder/results/results.json
 
-# delete the solution so you can't just include it during student-compile time
-mkdir tmp
-cp /autograder/source/src/solution/*.out tmp
-rm tmp/a.out
-rm -rf /autograder/source/src/solution
-mkdir /autograder/source/src/solution
-cp tmp/*.out /autograder/source/src/solution
+# setup permissions for grading
+find / -type f -exec chmod 640 {}
+adduser student --no-create-home --disabled-password --gecos ""
