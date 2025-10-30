974c6787b66f506ee9f01c51d66357a55157225c
NovaSagittarii <novasagittarii@gmail.com>
2025-10-30 04:58:29 -0600

    remove some more filesystem permissions

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 ### Installing dependencies ###
-apt install -y g++ python3 python3-pip
+# apt install -y g++ python3 python3-pip
 pip3 install pyseccomp
 
 ### Generating answer files (ahead of time for faster grading). ###
@@ -14,8 +14,11 @@ python3 exec.py solution
 # clean up side-effect of running compile.py and exec.py
 echo > /autograder/results/results.json
 
-# setup permissions for grading
-find /usr/bin -type f -exec chmod 750 {} \; > /dev/null 2>&1
-find /autograder -type f -exec chmod 750 {} \; > /dev/null 2>&1
+### setup permissions for grading ###
+find /autograder /gradescope /usr/bin -type f -exec chmod 750 {} \; > /dev/null 2>&1
+# remove any exec in var and lib (stuff in /usr is needed for python to work tho)
+find /var /lib -type f -perm /001 -exec chmod o-x {} \; 2>/dev/null
+# remove any write perms
+find / -type d -perm /002 -exec chmod o-w {} \; 2>/dev/null
 chmod 755 $(which python3) /usr/bin/env $(which bash)
 adduser student --no-create-home --disabled-password --gecos ""
