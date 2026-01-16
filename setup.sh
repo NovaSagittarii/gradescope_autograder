e37a95b8ca8e1ba9df9783d54719b40957a9209d
NovaSagittarii <novasagittarii@gmail.com>
2026-01-15 21:48:09 -0700

    cleanup comments in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 ### Installing dependencies ###
 # apt install -y g++ python3 python3-pip
+# ^^^ these are already available in gradescope base image
+
 pip3 install pyseccomp
 
 # create the student user early (since exec.py runs using restricted user)
@@ -19,8 +21,15 @@ echo > /autograder/results/results.json
 
 ### setup permissions for grading ###
 find /autograder /gradescope /usr/bin -type f -exec chmod 750 {} \; > /dev/null 2>&1
+
 # remove any exec in var and lib (stuff in /usr is needed for python to work tho)
 find /var /lib -type f -perm /001 -exec chmod o-x {} \; 2>/dev/null
+
+# remove any exec on writable files
+# find / -type f -perm -007 -exec chmod o-x {} \; 2>/dev/null
+
 # remove any write perms
 find / -type d -perm /002 -exec chmod o-w {} \; 2>/dev/null
+
+# restricted user still needs to execute files to run student code
 chmod 755 $(which python3) /usr/bin/env $(which bash)
