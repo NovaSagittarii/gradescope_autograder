18f6c28d80beb6040d670d15a085465be4e13517
NovaSagittarii <novasagittarii@gmail.com>
2026-01-18 00:31:39 -0700

    fix environ not persisting to task generation

--- src/exec.py
+++ src/exec.py
@@ -33,9 +33,10 @@ for network in [socket.AF_INET, socket.AF_INET6]:
     )
 filter.load()
 
+tasks = task_details.generate_tests().items()  # generate before environ.clear()
 os.environ.clear()  # remove any env that might be loaded
 
-for id, payload in task_details.generate_tests().items():
+for id, payload in tasks:
     assert id != "a", "a.out is reserved for the binary"
     # pass as base64 from stdin to avoid any escaping character issues
     base64payload = base64.b64encode(payload.data.encode()).decode()
